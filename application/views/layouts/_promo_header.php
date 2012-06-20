926a66aaf107153f0591900b6888b25c5125eec0
Attila Dobi <hello@adobi.hu>
2012-06-20 15:45:22 +0200

    google analytics tracking code

--- application/views/layouts/_promo_header.php
+++ application/views/layouts/_promo_header.php
@@ -28,4 +28,19 @@
     </head>
     
     <body>    
+        <script type="text/javascript">
+        
+            var _gaq = _gaq || [];
+            _gaq.push(['_setAccount', 'UA-27571060-2']);
+            _gaq.push(['_setSiteSpeedSampleRate', 100]);
+            _gaq.push(['_setDomainName', 'crosspromo.invictus.com']);
+            _gaq.push(['_trackPageview']);
+            
+            (function() {
+                var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+                ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+                var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+            })();
+      
+        </script>    
         <div class="container <?php echo isset($params['game']) ? 'well' : '' ?>" id="container">
